ab71da21bde2328c8a40e661a4bd27b8aaa4f415
PZrobek <37156@studentwsei.pl>
2022-06-15 20:43:00 +0200

    Lab2b - Podłączenie zdarzenia zmiany wybranej pozycji do instancji SelectionModel - uzupełnienie w metodzie initialize w kodzie inicjalizacji kontrolera.

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2a/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2a/Controller.java
@@ -1,18 +1,29 @@
 package pl.lublin.wsei.java.cwiczenia.lab2a;
 
 import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 
 public class Controller {
     public Label lbFile;
+    public Button btnOtworzPlik;
+    public TextField txtAdresStrony;
+    public Button btnPrzejdzDoStrony;
+    public Button btnPokazInfografike;
+    public ImageView imgMiniaturka;
+
 
     FileChooser fileChooser = new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)","*.xml");
@@ -24,6 +35,18 @@ public class Controller {
     @FXML
     public void initialize(){
         fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(
+            new ChangeListener<Number>(){
+                @Override
+                public void changed(ObservableValue<? extends Number> observableValue,
+                                    Number old_val, Number new_val){
+                    int index = new_val.intValue();
+                    if (index != -1){
+                        txtAdresStrony.setText(igList.infografiki.get(index).adresStrony);
+                    }
+                }
+            }
+        );
     }
 
     public void btnOpenFileAction(ActionEvent actionEvent){
